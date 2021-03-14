98434499459190b0c67522c8558f58fc4ab113a5
Andreas Coroiu <andreas.coroiu@gmail.com>
2021-03-14 18:29:43 +0100

    Add active device.

--- src/storage/Storage.h
+++ src/storage/Storage.h
@@ -23,14 +23,23 @@ public:
     }
   }
 
+  Device getActiveDevice()
+  {
+    return mapToDevice(document["activeDevice"]);
+  }
+
+  void setActiveDevice(Device device)
+  {
+    document["activeDevice"] = mapToJson(device);
+    serialize();
+  }
+
   std::vector<Device> getDevices()
   {
     std::vector<Device> toReturn;
     for (JsonVariant v : document["devices"].to<JsonArray>())
     {
-      toReturn.push_back(Device{
-          .address = v["address"].as<std::string>(),
-          .name = v["name"].as<std::string>()});
+      toReturn.push_back(mapToDevice(v));
     }
 
     return toReturn;
@@ -38,16 +47,14 @@ public:
 
   void addDevice(Device device)
   {
-    JsonObject json;
-    json["address"] = device.address.toString();
-    json["name"] = device.name;
-    document["devices"].add(json);
+    document["devices"].add(mapToJson(device));
     serialize();
   }
 
 private:
   void init()
   {
+    document["activeDevice"] = mapToJson(Device());
     document.createNestedArray("devices");
   }
 
@@ -67,6 +74,20 @@ private:
     nvs.setBlob("configuration", output, size);
   }
 
+  Device mapToDevice(JsonVariant v)
+  {
+    return Device{
+        .address = v["address"].as<std::string>(),
+        .name = v["name"].as<std::string>()};
+  }
+
+  JsonObject mapToJson(Device d)
+  {
+    JsonObject json;
+    json["address"] = d.address.toString();
+    json["name"] = d.name;
+    return json;
+  }
 } storage;
 
 extern Storage storage;
